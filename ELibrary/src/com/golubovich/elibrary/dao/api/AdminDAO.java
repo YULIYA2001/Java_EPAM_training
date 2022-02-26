30505e801666e6c10dab41801033fc1b6ba99bd0
yuliya <gyuliya2001@gmail.com>
2022-02-27 00:07:34 +0300

    made better realisation adminDAO

--- ELibrary/src/com/golubovich/elibrary/dao/api/AdminDAO.java
+++ ELibrary/src/com/golubovich/elibrary/dao/api/AdminDAO.java
@@ -1,12 +1,10 @@
 package com.golubovich.elibrary.dao.api;
 
 import com.golubovich.elibrary.beans.Admin;
-import com.golubovich.elibrary.exceptions.DAOException;
 
 public interface AdminDAO {
-    boolean update(String var1, String var2, String var3, String var4);
-
-    boolean authorization(String var1) throws DAOException;
-
     Admin read();
+    boolean update(Admin updatedAdmin);// throws DAOException;
+
+    Admin findByPassword(String password);// throws DAOException;
 }
--- ELibrary/src/com/golubovich/elibrary/dao/impl/AdminDAOImpl.java
+++ ELibrary/src/com/golubovich/elibrary/dao/impl/AdminDAOImpl.java
@@ -2,41 +2,24 @@ package com.golubovich.elibrary.dao.impl;
 
 import com.golubovich.elibrary.beans.Admin;
 import com.golubovich.elibrary.dao.api.AdminDAO;
-import com.golubovich.elibrary.exceptions.DAOException;
 import com.golubovich.elibrary.source.DataSource;
 
 public class AdminDAOImpl implements AdminDAO {
     private final DataSource dataSource = DataSource.getInstance();
 
-    public AdminDAOImpl() {
+    public Admin read() {
+        return dataSource.getAdmin();
     }
 
-    public boolean update(String surname, String name, String patronymic, String password) {
-        if (!surname.equals("-")) {
-            this.dataSource.getAdmin().setSurname(surname);
-        }
-
-        if (!name.equals("-")) {
-            this.dataSource.getAdmin().setName(name);
-        }
-
-        if (!patronymic.equals("-")) {
-            this.dataSource.getAdmin().setPatronymic(patronymic);
-        }
-
-        if (!password.equals("-")) {
-            this.dataSource.getAdmin().setPassword(password);
-        }
-
+    public boolean update(Admin updatedAdmin) {
+        dataSource.setAdmin(updatedAdmin);
         return true;
     }
 
-    public boolean authorization(String password) throws DAOException {
-        return this.dataSource.getAdmin().getPassword().equals(password);
-    }
-
-    public Admin read() {
-        return this.dataSource.getAdmin();
+    public Admin findByPassword(String password) {
+        if (dataSource.getAdmin().getPassword().equals(password))
+            return dataSource.getAdmin();
+        return null;
     }
 }
 
