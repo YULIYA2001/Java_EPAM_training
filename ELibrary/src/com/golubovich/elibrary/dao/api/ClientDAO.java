62b3648e98d3cd4e5aa71640fa6c71a72547fd95
yuliya <gyuliya2001@gmail.com>
2022-02-27 01:21:47 +0300

    made better realisation clientDAO

--- ELibrary/src/com/golubovich/elibrary/dao/api/ClientDAO.java
+++ ELibrary/src/com/golubovich/elibrary/dao/api/ClientDAO.java
@@ -1,21 +1,13 @@
 package com.golubovich.elibrary.dao.api;
 
 import com.golubovich.elibrary.beans.Client;
-import com.golubovich.elibrary.enums.ClientStatus;
 import java.util.List;
 
 public interface ClientDAO {
-    boolean authorize(String var1, String var2);
-
     boolean create(Client var1);
+    List<Client> read();
+    boolean update(Client updatedClient);
+    boolean delete(Client deletedClient);
 
-    boolean update(String var1, String var2, String var3, String var4, String var5, ClientStatus var6);
-
-    boolean delete(String var1);
-
-    Client read(String var1);
-
-    List<Client> readAll();
-
-    boolean isClient(String var1);
+    Client findByEmail(String eMail);
 }
--- ELibrary/src/com/golubovich/elibrary/dao/impl/ClientDAOImpl.java
+++ ELibrary/src/com/golubovich/elibrary/dao/impl/ClientDAOImpl.java
@@ -2,7 +2,6 @@ package com.golubovich.elibrary.dao.impl;
 
 import com.golubovich.elibrary.beans.Client;
 import com.golubovich.elibrary.dao.api.ClientDAO;
-import com.golubovich.elibrary.enums.ClientStatus;
 import com.golubovich.elibrary.source.DataSource;
 import java.util.Iterator;
 import java.util.List;
@@ -10,112 +9,45 @@ import java.util.List;
 public class ClientDAOImpl implements ClientDAO {
     private final DataSource dataSource = DataSource.getInstance();
 
-    public ClientDAOImpl() {
-    }
-
-    public boolean authorize(String eMail, String password) {
-        Iterator var3 = this.dataSource.getClients().iterator();
-
-        Client client;
-        do {
-            if (!var3.hasNext()) {
-                return false;
-            }
-
-            client = (Client)var3.next();
-        } while(!client.geteMail().equals(eMail) || !client.getPassword().equals(password));
-
-        return true;
-    }
-
     public boolean create(Client client) {
-        boolean result = this.dataSource.getClients().add(client);
-        return result;
+        return dataSource.getClients().add(client);
     }
 
-    public boolean update(String eMail, String surname, String name, String patronymic, String password, ClientStatus clientStatus) {
-        Iterator var7 = this.dataSource.getClients().iterator();
-
-        Client client;
-        do {
-            if (!var7.hasNext()) {
-                return false;
-            }
-
-            client = (Client)var7.next();
-        } while(!client.geteMail().equals(eMail));
-
-        if (!surname.equals("-")) {
-            client.setSurname(surname);
-        }
-
-        if (!name.equals("-")) {
-            client.setName(name);
-        }
-
-        if (!patronymic.equals("-")) {
-            client.setPatronymic(patronymic);
-        }
-
-        if (!password.equals("-")) {
-            client.setPassword(password);
-        }
-
-        if (clientStatus != ClientStatus.UNDEFINED) {
-            client.setClientStatus(clientStatus);
-        }
-
-        return true;
-    }
-
-    public boolean delete(String eMail) {
-        Iterator iterator = this.dataSource.getClients().iterator();
-
-        Client client;
-        do {
-            if (!iterator.hasNext()) {
-                return false;
-            }
-
-            client = (Client)iterator.next();
-        } while(!client.geteMail().equals(eMail));
-
-        this.dataSource.getClients().remove(client);
-        return true;
-    }
-
-    public Client read(String eMail) {
-        Iterator var2 = this.dataSource.getClients().iterator();
-
-        Client client;
-        do {
-            if (!var2.hasNext()) {
-                return null;
-            }
-
-            client = (Client)var2.next();
-        } while(!client.geteMail().equals(eMail));
-
-        return client;
-    }
-
-    public List<Client> readAll() {
+    public List<Client> read() {
         return this.dataSource.getClients();
     }
 
-    public boolean isClient(String eMail) {
-        Iterator var2 = this.dataSource.getClients().iterator();
+    public boolean update(Client updatedClient) {
+        Client currentClient = findByEmail(updatedClient.getEMail());
 
+        if (currentClient != null) {
+            dataSource.getClients().set(dataSource.getClients().indexOf(currentClient), updatedClient);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(Client deletedClient) {
+        Client currentClient = findByEmail(deletedClient.getEMail());
+
+        if (currentClient != null) {
+            dataSource.getClients().remove(currentClient);
+            return true;
+        }
+        return false;
+    }
+
+    public Client findByEmail(String eMail) {
+        Iterator<Client> clientsIterator = dataSource.getClients().iterator();
         Client client;
-        do {
-            if (!var2.hasNext()) {
-                return false;
+
+        while(clientsIterator.hasNext()) {
+            client = clientsIterator.next();
+            if (client.getEMail().equals(eMail)) {
+                return client;
             }
-
-            client = (Client)var2.next();
-        } while(!client.geteMail().equals(eMail));
-
-        return true;
+        }
+        return null;
     }
 }
 
