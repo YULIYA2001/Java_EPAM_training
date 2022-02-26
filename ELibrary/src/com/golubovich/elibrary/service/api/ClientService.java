d490f82ff8543b1d6a986e7fb3ac3429d1b6806d
yuliya <gyuliya2001@gmail.com>
2022-02-27 01:26:42 +0300

    made better realisation clientService

--- ELibrary/src/com/golubovich/elibrary/service/api/ClientService.java
+++ ELibrary/src/com/golubovich/elibrary/service/api/ClientService.java
@@ -3,15 +3,12 @@ package com.golubovich.elibrary.service.api;
 import com.golubovich.elibrary.enums.ClientStatus;
 
 public interface ClientService {
-    boolean authorize(String var1, String var2);
-
-    boolean register(String[] var1);
-
-    boolean changeData(String var1, String var2, String var3, String var4, String var5, ClientStatus var6);
-
+    boolean register(String[] orderedFields);
+    String showAll();
+    boolean changeData(String eMail, String surname, String name, String patronymic,
+                       String password, ClientStatus clientStatus);
     boolean deleteByEMail(String var1);
 
-    String findByEMail(String var1);
-
-    String showAll();
+    boolean authorize(String eMail, String password);
+    String showByEMail(String eMail);
 }
--- ELibrary/src/com/golubovich/elibrary/service/impl/ClientServiceImpl.java
+++ ELibrary/src/com/golubovich/elibrary/service/impl/ClientServiceImpl.java
@@ -1,79 +1,112 @@
 package com.golubovich.elibrary.service.impl;
 
+
 import com.golubovich.elibrary.beans.Client;
 import com.golubovich.elibrary.dao.DAOProvider;
 import com.golubovich.elibrary.dao.api.ClientDAO;
 import com.golubovich.elibrary.enums.ClientStatus;
 import com.golubovich.elibrary.service.api.ClientService;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 public class ClientServiceImpl implements ClientService {
     private final DAOProvider provider = DAOProvider.getInstance();
+    private final ClientDAO clientDAO = provider.getClientDAO();
 
-    public ClientServiceImpl() {
-    }
+    public boolean register(String[] orderedFields) {
 
-    public boolean authorize(String eMail, String password) {
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        boolean result = clientDAO.authorize(eMail, password);
-        return result;
-    }
+        String surname = orderedFields[0];
+        String name = orderedFields[1];
+        String patronymic = orderedFields[2];
+        String password = orderedFields[3];
+        int age = Integer.parseInt(orderedFields[4]);
+        String eMail = orderedFields[5];
 
-    public boolean register(String[] orderedParams) {
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        if ("-".equals(orderedParams[2])) {
-            orderedParams[2] = "";
+        if ("-".equals(patronymic)) {
+            patronymic = "";
         }
 
-        Client client = new Client(orderedParams[0], orderedParams[1], orderedParams[2], orderedParams[3], Integer.parseInt(orderedParams[4]), orderedParams[5], new Date(), ClientStatus.BEGINNER);
-        if (!clientDAO.isClient(client.geteMail())) {
-            boolean result = clientDAO.create(client);
-            return result;
-        } else {
-            return false;
+        Client client = new Client(surname, name, patronymic, password, age, eMail,
+                new Date(), ClientStatus.BEGINNER);
+
+        if (clientDAO.findByEmail(eMail) == null) {
+            return clientDAO.create(client);
         }
+        return false;
     }
 
-    public boolean changeData(String eMail, String surname, String name, String patronymic, String password, ClientStatus clientStatus) {
+    public String showAll() {
+        List<Client> clients = clientDAO.read();
+
+        if (clients != null && !clients.isEmpty()) {
+            return objectListToString(clients);
+        }
+        return "Нет зарегистрированных клиентов";
+    }
+
+    public boolean changeData(String eMail, String surname, String name, String patronymic,
+                              String password, ClientStatus clientStatus) {
         if (patronymic == null) {
             patronymic = "";
         }
 
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        boolean result = clientDAO.update(eMail, surname, name, patronymic, password, clientStatus);
-        return result;
+        Client client = clientDAO.findByEmail(eMail);
+
+        if (client != null) {
+            if (!surname.equals("-")){
+                client.setSurname(surname);
+            }
+            if (!name.equals("-")) {
+                client.setName(name);
+            }
+            if (!patronymic.equals("-")) {
+                client.setPatronymic(patronymic);
+            }
+            if (!password.equals("-")) {
+                client.setPassword(password);
+            }
+            if (clientStatus != ClientStatus.UNDEFINED) {
+                client.setClientStatus(clientStatus);
+            }
+            return clientDAO.update(client);
+        }
+        return false;
     }
 
     public boolean deleteByEMail(String eMail) {
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        boolean result = clientDAO.delete(eMail);
-        return result;
-    }
-
-    public String findByEMail(String eMail) {
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        Client client = clientDAO.read(eMail);
-        return client != null ? client.toReadableString() : "error Client not found";
-    }
-
-    public String showAll() {
-        ClientDAO clientDAO = this.provider.getClientDAO();
-        List<Client> clients = clientDAO.readAll();
-        if (clients != null && !clients.isEmpty()) {
-            StringBuilder response = new StringBuilder();
-            Iterator var4 = clients.iterator();
-
-            while(var4.hasNext()) {
-                Client client = (Client)var4.next();
-                response.append(" ").append(client.toReadableString()).append("\n");
-            }
-
-            return String.valueOf(response);
-        } else {
-            return "Нет зарегистрированных клиентов";
+        Client deletedClient = clientDAO.findByEmail(eMail);
+        if (deletedClient != null) {
+            return clientDAO.delete(deletedClient);
         }
+        return false;
+    }
+
+    public String showByEMail(String eMail) {
+        Client client = clientDAO.findByEmail(eMail);
+        if (client != null) {
+            return client.toString();
+        }
+        return "1 error Client not found | class ClientServiceImpl method showByEmail";
+    }
+
+    public boolean authorize(String eMail, String password) {
+        Client client = clientDAO.findByEmail(eMail);
+
+        if (client != null) {
+            return client.getPassword().equals(password);
+        }
+        return false;
+    }
+
+
+    private String objectListToString(List<Client> clients) {
+        StringBuilder clientsString = new StringBuilder();
+
+        for (Client client : clients) {
+            clientsString.append(" ").append(client.toString()).append("\n");
+        }
+
+        return String.valueOf(clientsString).substring(0, clientsString.length() - 1);
     }
 }
 
