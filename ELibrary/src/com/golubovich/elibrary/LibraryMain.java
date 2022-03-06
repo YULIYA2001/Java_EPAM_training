4286cc3d4d2cf50f6403f3738877eda846f5f4e2
yuliya <gyuliya2001@gmail.com>
2022-03-07 00:32:16 +0300

    add serialization

--- ELibrary/src/com/golubovich/elibrary/LibraryMain.java
+++ ELibrary/src/com/golubovich/elibrary/LibraryMain.java
@@ -1,11 +1,19 @@
 package com.golubovich.elibrary;
 
-import static com.golubovich.elibrary.util.Constants.WRONG_INPUT;
+import static com.golubovich.elibrary.utils.Constants.SERIALIZATION_FILE_NAME;
+import static com.golubovich.elibrary.utils.Constants.WRONG_INPUT;
 
-import com.golubovich.elibrary.util.Initializer;
+import com.golubovich.elibrary.beans.Library;
+import com.golubovich.elibrary.utils.Initializer;
+import com.golubovich.elibrary.utils.Serialization;
 import com.golubovich.elibrary.view.AdminMenu;
 import com.golubovich.elibrary.view.ClientMenu;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Scanner;
+
 
 public class LibraryMain {
 
@@ -14,7 +22,10 @@ public class LibraryMain {
         final String ADMIN = "2";
         final String EXIT = "0";
 
-        Initializer.initialization();
+
+        Serialization.deserialize();
+
+
 
         Scanner in = new Scanner(System.in);
         System.out.println("\t Система Онлайн-Библиотека\n--------------------------------------");
@@ -31,6 +42,7 @@ public class LibraryMain {
                     AdminMenu.adminSignInMenu();
                     break;
                 case EXIT:
+                    Serialization.serialize();
                     return;
                 default:
                     System.out.println(WRONG_INPUT);
