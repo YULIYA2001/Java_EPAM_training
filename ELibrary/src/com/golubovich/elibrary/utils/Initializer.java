427c2d5c300b72fabbe3ea3355f43db482a281ae
yuliya <gyuliya2001@gmail.com>
2022-03-07 00:39:26 +0300

    add library and admin assignment

--- ELibrary/src/com/golubovich/elibrary/utils/Initializer.java
+++ ELibrary/src/com/golubovich/elibrary/utils/Initializer.java
@@ -1,17 +1,18 @@
 package com.golubovich.elibrary.utils;
 
+import com.golubovich.elibrary.beans.Admin;
 import com.golubovich.elibrary.beans.Book;
 import com.golubovich.elibrary.beans.Client;
 import com.golubovich.elibrary.beans.EducationalMaterial;
 import com.golubovich.elibrary.beans.Genre;
 import com.golubovich.elibrary.beans.Item;
+import com.golubovich.elibrary.beans.Library;
 import com.golubovich.elibrary.beans.Magazine;
 import com.golubovich.elibrary.enums.ClientStatus;
 import com.golubovich.elibrary.enums.EdMaterialSubjects;
 import com.golubovich.elibrary.enums.EdMaterialType;
 import com.golubovich.elibrary.enums.ItemType;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,6 +22,13 @@ import java.util.Map;
 public class Initializer {
 
     public static void initialization() {
+        DataSource.getInstance().setLibrary(
+            new Library("E-Library", "http://elibrary.com", "e-library@gmail.com")
+        );
+        DataSource.getInstance().setAdmin(
+            new Admin("admin", "admin", "", "admin123")
+        );
+
         ArrayList<Genre> genres = new ArrayList();
         genres.add(new Genre("Юмор", "смешное художественное произведение"));
         genres.add(new Genre("Роман", "художественное произведение большого объема"));
@@ -65,8 +73,8 @@ public class Initializer {
                 "Толстой Л.Н.")
         );
 
-        ArrayList<Item> edMaterial = new ArrayList();
-        edMaterial.add(new EducationalMaterial(
+        ArrayList<Item> edMaterials = new ArrayList();
+        edMaterials.add(new EducationalMaterial(
                 "Биология 5 кл.",
                 new ArrayList(),
                 "русский",
@@ -87,7 +95,7 @@ public class Initializer {
         Map<ItemType, ArrayList<Item>> items = new HashMap();
         items.put(ItemType.BOOK, books);
         items.put(ItemType.MAGAZINE, magazines);
-        items.put(ItemType.EDUCATIONAL_MATERIAL, edMaterial);
+        items.put(ItemType.EDUCATIONAL_MATERIAL, edMaterials);
 
         DataSource.getInstance().setItems(items);
     }
