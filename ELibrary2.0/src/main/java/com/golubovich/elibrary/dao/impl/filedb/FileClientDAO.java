9aff382aa3ab2a736547da4c29e296f27b336c36
yuliya <gyuliya2001@gmail.com>
2022-03-08 20:32:25 +0300

    add googlestyle

--- ELibrary2.0/src/main/java/com/golubovich/elibrary/dao/impl/filedb/FileClientDAO.java
+++ ELibrary2.0/src/main/java/com/golubovich/elibrary/dao/impl/filedb/FileClientDAO.java
@@ -19,101 +19,97 @@ import java.util.Scanner;
 
 public class FileClientDAO implements ClientDAO {
 
-    public boolean create(Client client) throws DAOException {
-        try(FileWriter fw = new FileWriter(CLIENT_DB_TABLE, true))
-        {
-            String jsonClientString = JSONSerializer.toJSON(client);
-            fw.write(jsonClientString + '\n');
-            fw.flush();
-            return true;
-        }
-        catch(IOException e){
-            throw new DAOException(e);
-        }
+  public boolean create(Client client) throws DAOException {
+    try (FileWriter fw = new FileWriter(CLIENT_DB_TABLE, true)) {
+      String jsonClientString = JSONSerializer.toJSON(client);
+      fw.write(jsonClientString + '\n');
+      fw.flush();
+      return true;
+    } catch (IOException e) {
+      throw new DAOException(e);
+    }
+  }
+
+  public List<Client> read() throws DAOException {
+    ArrayList<Client> clients = new ArrayList<>();
+
+    try (Scanner scanner = new Scanner(new File(CLIENT_DB_TABLE))) {
+      while (scanner.hasNextLine()) {
+        clients.add(JSONSerializer.fromJSON(scanner.nextLine().trim(), Client.class));
+        Client.decrementCount();
+      }
+    } catch (FileNotFoundException e) {
+      throw new DAOException(e);
     }
 
-    public List<Client> read() throws DAOException {
-        ArrayList<Client> clients = new ArrayList<>();
+    // Client.getCount();
+    return clients;
+  }
 
-        try (Scanner scanner = new Scanner(new File(CLIENT_DB_TABLE))) {
-            while (scanner.hasNextLine()) {
-                clients.add(JSONSerializer.fromJSON(scanner.nextLine().trim(), Client.class));
-                Client.decrementCount();
-            }
-        } catch (FileNotFoundException e) {
-            throw new DAOException(e);
+  public void update(Client currentClient, Client updatedClient) throws DAOException {
+    String jsonCurrentClientString = JSONSerializer.toJSON(currentClient);
+    String jsonUpdatedClientString = JSONSerializer.toJSON(updatedClient);
+    File inputFile = new File(CLIENT_DB_TABLE);
+    File temporaryFile = new File("temp.json");
+
+    try (BufferedReader fr = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter fw = new BufferedWriter(new FileWriter(temporaryFile))) {
+
+      String s;
+      while ((s = fr.readLine()) != null) {
+        if (s.equals(jsonCurrentClientString)) {
+          fw.write(jsonUpdatedClientString);
+        } else {
+          fw.write(s);
         }
+        fw.newLine();
+      }
+      inputFile.delete();
+      temporaryFile.renameTo(inputFile);
 
-        // Client.getCount();
-        return clients;
+    } catch (IOException e) {
+      throw new DAOException(e);
     }
+  }
 
-    public void update(Client currentClient, Client updatedClient) throws DAOException {
-        String jsonCurrentClientString = JSONSerializer.toJSON(currentClient);
-        String jsonUpdatedClientString = JSONSerializer.toJSON(updatedClient);
-        File inputFile = new File(CLIENT_DB_TABLE);
-        File temporaryFile = new File("temp.json");
+  public boolean delete(Client deletedClient) throws DAOException {
+    String jsonDeletedClientString = JSONSerializer.toJSON(deletedClient);
+    File inputFile = new File(CLIENT_DB_TABLE);
+    File temporaryFile = new File("temp.json");
 
-        try (BufferedReader fr = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter fw = new BufferedWriter(new FileWriter(temporaryFile))) {
+    try (BufferedReader fr = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter fw = new BufferedWriter(new FileWriter(temporaryFile))) {
 
-            String s;
-            while((s = fr.readLine()) != null){
-                if (s.equals(jsonCurrentClientString)) {
-                    fw.write(jsonUpdatedClientString);
-                }
-                else {
-                    fw.write(s);
-                }
-                fw.newLine();
-            }
-            inputFile.delete();
-            temporaryFile.renameTo(inputFile);
-
-        } catch (IOException e) {
-            throw new DAOException(e);
+      String s;
+      while ((s = fr.readLine()) != null) {
+        if (s.equals(jsonDeletedClientString)) {
+          continue;
+        } else {
+          fw.write(s);
         }
+        fw.newLine();
+      }
+      return inputFile.delete() && temporaryFile.renameTo(inputFile);
+
+    } catch (IOException e) {
+      throw new DAOException(e);
     }
+  }
 
-    public boolean delete(Client deletedClient) throws DAOException {
-        String jsonDeletedClientString = JSONSerializer.toJSON(deletedClient);
-        File inputFile = new File(CLIENT_DB_TABLE);
-        File temporaryFile = new File("temp.json");
+  public Client findByEmail(String eMail) throws DAOException {
+    try (Scanner scanner = new Scanner(new File(CLIENT_DB_TABLE))) {
+      while (scanner.hasNextLine()) {
+        Client client = JSONSerializer.fromJSON(scanner.nextLine().trim(), Client.class);
+        Client.decrementCount();
 
-        try (BufferedReader fr = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter fw = new BufferedWriter(new FileWriter(temporaryFile))) {
-
-            String s;
-            while((s = fr.readLine()) != null){
-                if (s.equals(jsonDeletedClientString)) {
-                    continue;
-                }
-                else {
-                    fw.write(s);
-                }
-                fw.newLine();
-            }
-            return inputFile.delete() && temporaryFile.renameTo(inputFile);
-
-        } catch (IOException e) {
-            throw new DAOException(e);
+        if (client.getEMail().equals(eMail)) {
+          return client;
         }
+      }
+      return null;
+    } catch (FileNotFoundException e) {
+      throw new DAOException(e);
     }
-
-    public Client findByEmail(String eMail) throws DAOException {
-        try (Scanner scanner = new Scanner(new File(CLIENT_DB_TABLE))) {
-            while (scanner.hasNextLine()) {
-                Client client = JSONSerializer.fromJSON(scanner.nextLine().trim(), Client.class);
-                Client.decrementCount();
-
-                if (client.getEMail().equals(eMail)) {
-                    return client;
-                }
-            }
-            return null;
-        } catch (FileNotFoundException e) {
-            throw new DAOException(e);
-        }
-    }
+  }
 }
 
